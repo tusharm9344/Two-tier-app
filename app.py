b85ca869455f021241051609d54b6941ff495836
tusharm9344 <tushar.kakrola@gmail.com>
2026-06-21 13:50:29 +0000

    fix: create tables on startup

--- app.py
+++ app.py
@@ -39,3 +39,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # creates tables on startup
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+with app.app_context():
+    db.create_all()
